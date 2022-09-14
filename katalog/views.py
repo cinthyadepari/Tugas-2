5f8d563f93620ac121f85b5acd34c92b828bd8d8
Cinthya Depari <cinthya.yosephine@ui.ac.id>
2022-09-14 11:53:54 +0700

    Tugas 2 selesai

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Cinthya Depari',
+        'NPM' : '2106751493'
+    }
+    return render(request, "katalog.html", context)
